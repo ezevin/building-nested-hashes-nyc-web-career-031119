cc42f09d7699ba97a1245f26f9421be525e983a3
Elizabeth Zevin <ehzevin@gmail.com>
2019-02-14 19:45:01 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -21,8 +21,7 @@ def bonus
   }
 
   #code your solution here:
-epic_tragedy.delete[:montague] = {:hero => status:}
-
+epic_tragedy.delete[:montague] = :hero => status:
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
   epic_tragedy
