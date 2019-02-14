872f322560ca0dca2b00d6103c2d05f2aa022691
Elizabeth Zevin <ehzevin@gmail.com>
2019-02-14 19:38:04 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -22,7 +22,7 @@ def bonus
 
   #code your solution here:
 epic_tragedy.delete[:montague][:hero][:status] = "dead"
-epic_tragedy.delete[:capulet] = "dead"
+epic_tragedy.delete[:capulet][:heroine][:status] = "dead"
   
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
